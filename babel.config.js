27e7e7e4442c6106136a9ee6c899e254d656475e
Yan <yankunitski@gmail.com>
2019-01-03 21:28:20 +0300

    babel configured

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	presets: [
-	  [
-		"@babel/preset-env",
-		{
-		  targets: {
-			  ie: "10"
-		  }
-		}
-	  ]
-	]
-  }
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					ie: '10',
+				},
+			},
+		],
+	],
+};
