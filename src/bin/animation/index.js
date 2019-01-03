56346d00d9cb86241301ca790a69ab139c1c3ed9
Yan <yankunitski@gmail.com>
2019-01-03 21:31:08 +0300

    main animation

--- src/bin/animation/index.js
+++ src/bin/animation/index.js
@@ -2,35 +2,38 @@ const frames = require('./frames');
 const promise = require('../../libs/promise');
 const getDelay = require('../../libs/getDelay');
 const getTape = require('./getTape');
-
+const sendInfoToUser = require('../../libs/sendInfoToUser');
 
 module.exports = (direction, wrapper, options, activeSlide) => {
-	const { slideTape, navTape, tapeD } = getTape(direction, wrapper, activeSlide, options.dotIndex);
-	const delay = getDelay(options.slideAnimation.active, options.slideAnimation.next);
+	let delay;
+	try {
+		const { slideTape, navTape, tapeD } = getTape(direction, wrapper, activeSlide, options.dotIndex);
+		delay = getDelay(options.slideAnimation.active, options.slideAnimation.next);
 
-	if (slideTape.now === slideTape.next) return promise(0);
+		if (slideTape.now === slideTape.next) return promise(0);
 
-	frames(slideTape, (tapeD === 'next' ? options.slideAnimation : options.slideAnimationRev));
+		frames(slideTape, (tapeD === 'next' ? options.slideAnimation : options.slideAnimationRev));
 
-	if (wrapper.navBar) {
-		frames(navTape, {
-			active: [options.navBarStyle.dots.usual],
-			next: [options.navBarStyle.dots.active],
-		});
-	}
-	if (wrapper.arrows) {
-		frames({
-			now: { style: {} },
-			next: wrapper.arrows[`arrow${tapeD === 'next' ? 'Next' : 'Prev'}`].firstChild,
-		},
-		{
-			active: [{ '': '' }],
-			next: [
-				options.arrowsStyle.arrows.active,
-				options.arrowsStyle.arrows.usual,
-			],
-		});
-	}
+		if (wrapper.navBar) {
+			frames(navTape, {
+				active: [options.navBarStyle.dots.usual],
+				next: [options.navBarStyle.dots.active],
+			});
+		}
+		if (wrapper.arrows) {
+			frames({
+				now: { style: {} },
+				next: wrapper.arrows[`arrow${tapeD === 'next' ? 'Next' : 'Prev'}`].firstChild,
+			},
+			{
+				active: [{ '': '' }],
+				next: [
+					options.arrowsStyle.arrows.active,
+					options.arrowsStyle.arrows.usual,
+				],
+			});
+		}
+	} catch (err) { sendInfoToUser(`Animation error ${err}`); }
 
 	return promise(delay);
 };
