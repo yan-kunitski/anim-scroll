b5a9067807b9a2822f926d83925ba14955a859b8
Yan <yankunitski@gmail.com>
2019-01-03 21:33:04 +0300

    event handlers

--- src/bin/onEvent/index.js
+++ src/bin/onEvent/index.js
@@ -2,11 +2,14 @@ const scroll = require('./scroll');
 const click = require('./click');
 const touch = require('./touch');
 const hover = require('./hover');
+const sendInfoToUser = require('../../libs/sendInfoToUser');
 
 module.exports = (elements, options, activeSlide) => {
-	if (options.isMobile) touch(elements, options, activeSlide);
-	else scroll(elements, options, activeSlide);
+	try {
+		if (options.isMobile) touch(elements, options, activeSlide);
+		else scroll(elements, options, activeSlide);
 
-	hover(elements, options, activeSlide);
-	click(elements, options, activeSlide);
+		hover(elements, options, activeSlide);
+		click(elements, options, activeSlide);
+	} catch (err) { sendInfoToUser(`Event error ${err}`, 0); }
 };
