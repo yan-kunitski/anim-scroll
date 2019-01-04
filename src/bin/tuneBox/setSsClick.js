c694663dc5a3769cac4871052954d60ca198fff2
Yan <yankunitski@gmail.com>
2019-01-04 21:11:05 +0300

    fixed condition for recursion

--- src/bin/tuneBox/setSsClick.js
+++ src/bin/tuneBox/setSsClick.js
@@ -1,4 +1,4 @@
 module.exports = (box, value) => {
 	box.ssClick = value;
-	if (box.children[1]) for (const el of box.children) module.exports(el, value);
+	if (box.children[0]) for (const el of box.children) module.exports(el, value);
 };
