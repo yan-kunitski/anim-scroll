f306c6887b286990fe6021a67e52bd5112cc3943
Yan <yankunitski@gmail.com>
2019-01-17 14:42:03 +0300

    replaced setStyle(target) to setSLide.js

--- src/bin/init.js
+++ src/bin/init.js
@@ -1,15 +1,14 @@
 const sendInfo = require('../libs/sendInfoToUser');
 const getElmenet = require('../libs/getElement');
-const setStyle = require('../libs/setStyle');
 const { OptionsError } = require('../errors');
 
-module.exports = (target, options) => {
+module.exports = target => {
 	const box = getElmenet(target);
 
 	if (!box) {
 		sendInfo('There is some error in terget name', 0);
 		throw new OptionsError(`Your target [${target}] is not defined`);
-	} else setStyle(box, options);
+	}
 
 	return box;
 };
