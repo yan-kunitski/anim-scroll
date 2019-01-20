39f8c330c3b5c83dcab7254ab6fd589e9324f501
Yan <yankunitski@gmail.com>
2019-01-20 18:38:29 +0300

    set up env targets

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,13 @@
 module.exports = {
 	presets: [
-		[
-			'@babel/preset-env',
-			{
-				targets: {
-					ie: '9',
-				},
+		['@babel/preset-env', {
+			targets: {
+				edge: '13',
+				chrome: '50',
+				firefox: '53',
+				opera: '37',
+				safari: '10',
 			},
-		],
-	],
-	plugins: [
-		['@babel/plugin-transform-runtime', {
-			helpers: false,
-			corejs: false,
-			regenerator: true,
-			useESModules: false,
 		}],
 	],
 };
